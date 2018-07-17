4501728f763ca5c88a7c1d346aaa2fa428f3eb6b
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-07-17 17:28:02 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,10 +1,4 @@
-DB_NANE     = ""
-DB_PASS     = ""
-DB_HOST     = ""
-DB_USER     = ""
-
 DEBUG       = True
 AUTO_RELOAD = True
 PORT        = 8010
 HOST        = "127.0.0.1"
-SECRET_KEY  = "'A0Zr98j/3yXR~XHH!jmN]LWX/,?RT'"
